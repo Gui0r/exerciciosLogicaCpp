f64915e2478c07bf3d772e97438bb321670969a6
Nicolas <NKKipper@senacrs.com.br>
2024-03-26 19:21:06 -0300

    nao sei

---
+++ exercicio1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int a, b, soma;
+
+    printf("Digite um numero para somar: " );
+    scanf("%d", a);
+
+    printf("Digite um segundo para somar: " );
+    scanf("%d", b);
+    soma = a + b;
+    printf("Soma dos numeros: %d", soma );
+}
